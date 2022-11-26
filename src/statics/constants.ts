3a8ad21a8163ed568891fb908b526c579d5ed58f
mohamed2m2018 <mohamed2m2018@gmail.com>
2022-11-26 15:19:14 +0200

    Eslint fix

--- src/statics/constants.ts
+++ src/statics/constants.ts
@@ -1,24 +1,24 @@
 export const filterationValues = {
-  MALE_OPTION: "Male",
-  FAMALE_OPTION: "Female",
-  ALL_OPTION: "All",
+  MALE_OPTION: 'Male',
+  FAMALE_OPTION: 'Female',
+  ALL_OPTION: 'All',
 };
 
 export const tableHeaders = {
-  FIRST_HEADER: "Avatar",
-  SECOND_HEADER: "Name",
-  THIRD_HEADER: "Email",
+  FIRST_HEADER: 'Avatar',
+  SECOND_HEADER: 'Name',
+  THIRD_HEADER: 'Email',
 };
 
 export const mediaTypes = {
-  IMAGE: "image",
-  TEXT: "text",
+  IMAGE: 'image',
+  TEXT: 'text',
 };
 
-export const ENDPOINT = "https://randomuser.me/api";
+export const ENDPOINT = 'https://randomuser.me/api';
 
 export const colors = {
-  aliceBlue: "#F1F8FF",
-  onahau: "#C8E1FF",
-  white: "#fff",
+  aliceBlue: '#F1F8FF',
+  onahau: '#C8E1FF',
+  white: '#fff',
 };
